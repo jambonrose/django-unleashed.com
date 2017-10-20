dba402f1b4ef2e41010b292afd30c229f1473c03
Andrew Pinkham <code@andrewsforge.com>
2017-10-19 22:40:33 -0400

    🐎 Override Liquid highlight tag to reduce code output

---
+++ src/_plugins/highlight.rb
@@ -0,0 +1,15 @@
+# Based on MIT-licensed code from @pathawks
+# https://github.com/pathawks/pathawks.com/blob/master/_plugins/highlight.rb
+# Frozen-String-Literal: true
+# Encoding: UTF-8
+
+module Jekyll
+  module Tags
+    class HighlightBlock < Liquid::Block
+      def add_code_tag(code)
+        lang = @lang.to_s.gsub("+", "-").freeze
+        "<pre><code class='language-#{lang}' data-lang='#{lang}'>#{code.chomp}</code></pre>"
+      end
+    end
+  end
+end
